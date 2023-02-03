84f7cfb278ceb13be3e4676c3b7fb9e3edf05a91
Tobias Pak <97552958+TobiasPak@users.noreply.github.com>
2023-02-03 14:22:09 -0800

    Update PowerInteraction.cs

---
+++ Assets/Scripts/PowerInteraction.cs
@@ -0,0 +1,16 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class PowerInteraction : MonoBehaviour
+// {
+//     public GameObject powerUp;
+//     // Update is called once per frame
+//     void OnTriggerEnter2D(Collider2D collP)
+//     {
+//         if (collP.gameObject.name == "PowerUP")
+//         {
+//             powerUp.SetActive(false);
+//         }
+//     }
+// }
